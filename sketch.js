064f02c6378f1c96065a7ad4e9c4546aa67a5ae7
Agnese Bartolucci <agnesebartolucci505@gmail.com>
2019-09-30 13:37:54 +0200

    assignement_01

--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
 function preload(){
-  // put preload code here
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+  frameRate(30);
+  background(color('black'));
+
 }
 
 function draw() {
-  // put drawing code here
+
+  translate(width/2,height/2);
+  rotate(frameCount);
+
+  let angle = sin(frameCount) * 300;
+  let angletwo = sin(frameCount) * 650;
+
+  stroke(lerpColor(color('#ff7d48'), color('#7a52e3'), sin(frameCount)));
+  strokeWeight(1);
+  noFill();
+  rect(0, 0, angle, angle+90);
+  rect(0, 0, angle+90, angle);
+  line(0, 0, 0, angle);
+  line(0, 0, angle, 0);
+  line(0, 0, 0, -angle);
+  line(0, 0, -angle, 0);
+
+  fill(0,0,0,20);
+  ellipse(0,0,angletwo,angletwo);
+
 }
