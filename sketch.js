763d9bd5d5018b2d2ff7a3a3aae7ef295aaa4ee0
Agnese Bartolucci <agnesebartolucci505@gmail.com>
2020-01-14 16:07:21 +0100

    re-evaluation

--- sketch.js
+++ sketch.js
@@ -1,17 +1,13 @@
-function preload(){
-
-}
-
 function setup() {
   createCanvas(windowWidth,windowHeight);
   angleMode(DEGREES);
   rectMode(CENTER);
   frameRate(30);
-  background(color('black'));
-
 }
 
 function draw() {
+
+  background(0,20);
 
   translate(width/2,height/2);
   rotate(frameCount);
@@ -19,7 +15,7 @@ function draw() {
   let angle = sin(frameCount) * 300;
   let angletwo = sin(frameCount) * 650;
 
-  stroke(lerpColor(color('#ff7d48'), color('#7a52e3'), sin(frameCount)));
+  stroke(lerpColor(color('#00ffed'), color('#f00b51'), sin(frameCount)));
   strokeWeight(1);
   noFill();
   rect(0, 0, angle, angle+90);
@@ -28,8 +24,5 @@ function draw() {
   line(0, 0, angle, 0);
   line(0, 0, 0, -angle);
   line(0, 0, -angle, 0);
-
-  fill(0,0,0,20);
-  ellipse(0,0,angletwo,angletwo);
 
 }
